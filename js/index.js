e1a829ea9063e08d68fc0c35100eaedea7045c8c
Tafiqulislam <tafiqulislamnahim126@gmail.com>
2024-02-20 16:00:56 +0600

    add a success section and function

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 function buttonNext (){
-    console.log('play strt now')
+    const mainContainerSection = document.getElementById('main-container-section');
+    mainContainerSection.classList.add('hidden');
+
+    // show the success section
+    const successSection = document.getElementById('success-section');
+    successSection.classList.remove('hidden');
 }
 
 
